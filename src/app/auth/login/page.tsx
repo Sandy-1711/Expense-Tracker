6b74b62e9325e91400f15395fd55a69026f55fff
Sandy-1711 <sandy1711003@gmail.com>
2023-12-22 02:06:51 +0530

    ui change

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+    return (<form className=" w-[100vw] h-[100vh] flex flex-col justify-center items-center text-black">
+        <input placeholder="Email"></input>
+        <button>Login with email</button>
+    </form>)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,18 +25,17 @@ export default async function Page() {
     revalidatePath('/')
 
   }
-  return <div className="w-full h-[100vh] bg-black flex justify-center items-center flex-col gap-5">
-    <h2 className=" absolute top-10 left-1/2 -translate-x-1/2 text-4xl text-white font-semibold">Expense Tracker</h2>
+  return <div className="w-full h-[100vh] bg-slate-500 flex justify-center items-center flex-col gap-5">
+    <h2 className=" absolute top-20 left-1/2 -translate-x-1/2 text-4xl text-black font-semibold">Expense Tracker</h2>
     <form action={pushData} className="flex flex-col bg-slate-700 rounded-sm p-4 gap-2">
       <input className="text-black p-2" type="text" placeholder="Name" name="name"></input>
       <input className="p-2 text-black" type="number" placeholder="Expense" name="amount"></input>
       <button className="rounded-md py-1 px-5 bg-blue-500">Submit</button>
     </form>
     {data && data.map(function ({ id, name, amount }: any) {
-      // console.log(id);
-      return <div key={id} className="rounded-sm border border-white px-2 py-2 w-90 text-white flex justify-between items-center gap-2">
+      return <div key={id} className="rounded-sm border border-white px-2 py-2 w-90 text-black flex justify-between items-center gap-2">
         <div className=" w-60 flex justify-between">
-          <span className=" font-semibold">{name}</span>
+          <span className="text-white font-semibold">{name}</span>
           <span>${amount}</span>
         </div>
         <Button id={id} deleteData={deleteData} />
