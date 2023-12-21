c79a4ec9f0180f1312d3c45dbd50b35d9d4464d6
Sandy-1711 <sandy1711003@gmail.com>
2023-12-21 21:16:25 +0530

    UI Changes

--- src/app/Button.tsx
+++ src/app/Button.tsx
@@ -3,5 +3,5 @@ export default function Button({ id, deleteData }: any) {
     // console.log(id);
     return <button onClick={function () {
         deleteData(id);
-    }} className="px-2 py-5 bg-blue-500 text-white">Delete</button>
+    }} className="px-2 h-max py-2 bg-blue-500 text-white rounded-sm">Delete</button>
 }
