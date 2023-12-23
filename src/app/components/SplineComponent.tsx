0087d9b1595d3af950ea2b9edd26184cde9b92d6
Sandy-1711 <sandy1711003@gmail.com>
2023-12-23 16:18:16 +0530

    fixed spline component error

---
+++ src/app/components/SplineComponent.tsx
@@ -0,0 +1,6 @@
+'use client'
+import Spline from '@splinetool/react-spline';
+export default function SplineComponent() {
+    return <Spline scene="https://prod.spline.design/12nEg6ewzZwDgACW/scene.splinecode" />
+
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import { RegisterLink, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components"
+import SplineComponent from "./components/SplineComponent"
 export default function Page() {
   return <div className=" relative z-10 bg-white flex flex-col justify-center items-center text-black h-screen w-full">
     <div className=" -z-10 h-full absolute top-0 bottom-0 left-0 right-0 w-full">
-      <spline-viewer url="https://prod.spline.design/12nEg6ewzZwDgACW/scene.splinecode"></spline-viewer>
+      <SplineComponent/>
     </div>
     <div className="absolute top-12">
 
